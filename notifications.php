63c8bc41d9a6401cef3e5960e6ac6095435acf41
Massimo Ruggirello <massimoruggirello@gmail.com>
2017-11-04 18:55:02 +0100

    init Header Requirements

---
+++ notifications.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+Plugin Name: Notifications
+Plugin URI: https://github.com/Max93/notifications
+Description: Simple and flexible notifications manager.
+Version: 0.0.1
+Author: Massimo Ruggirello
+Author URI: https://github.com/Max93
+Text Domain: notifications
+License: GPLv2 or later.
+Copyright: Massimo Ruggirello
+*/
