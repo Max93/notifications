f221fb2e6018e3e2ffef6aa59ca00ee4d2a07ebd
Massimo Ruggirello <massimoruggirello@gmail.com>
2017-11-05 02:34:42 +0100

    register activation and uninstall hook

--- notifications.php
+++ notifications.php
@@ -11,3 +11,8 @@ Text Domain: notifications
 License: GPLv2 or later.
 Copyright: Massimo Ruggirello
 */
+
+include_once dirname( __FILE__ ) . '/init.php';
+
+register_activation_hook( __FILE__, ['Init', 'activation']);
+register_uninstall_hook( __FILE__, ['Init', 'uninstall']);
